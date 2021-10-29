5fe123c0db05f479a24e3b8017306b61d79a8ecc
sedayegin <86283908+sedayeginn@users.noreply.github.com>
2021-10-29 23:40:43 +0300

    Add project files.

---
+++ ApplicationCore/Entities/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace ApplicationCore.Entities
+{
+    public class Category
+    {
+        public Category()
+        {
+            Products = new Collection<Product>();
+        }
+        public int Id { get; set; }
+        public string CategoryName { get; set; }
+        public bool IsDeleted { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
---
+++ ApplicationCore/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.Entities
+{
+   public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        public bool IsDeleted { get; set; }
+        public string InnerBarcode { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
